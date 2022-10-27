e312fe88da631bbf017f27a850062f9201b9c322
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2022-10-27 09:44:07 -0700

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -1,7 +1,7 @@
 var pos = 0;
 let pageWidth = window.innerWidth;
 const pacArray = [
-  ["images/PacMan1.png", "images/PacMan2.png"],
+  ["PacMan1.png", "images/PacMan2.png"],
     ["images/PacMan3.png",
         "images/PacMan4.png"],
 ];
